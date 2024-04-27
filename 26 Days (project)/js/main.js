cf85d9493cb3674ecc2f1a90da01abc1fdd6482d
general <sgladkoskoka@gmail.com>
2024-04-27 18:52:52 +0300

    Days 26 finished project

--- 26 Days (project)/js/main.js
+++ 26 Days (project)/js/main.js
@@ -36,3 +36,67 @@ filterButton.addEventListener('click', function() {
     }
     create_country_card(countries);
 });
+
+const statring_button = document.querySelector(`#statring-button`)
+const anyWordButton = document.querySelector('#with-any-word-button');
+const inputEl = document.querySelector(`#input-el`);
+const starts_or_contain = document.querySelector(`.starts-or-contain`)
+
+let startsWith = false;
+let anyWord = false;
+
+statring_button.addEventListener(`click`,function() {
+    startsWith = true;
+    anyWord = false;
+    if(startsWith) {
+        statring_button.style.backgroundColor = `rgb(164, 89, 255)`
+        statring_button.style.padding = `0.6rem`
+        statring_button.style.fontWeight = `bold`
+        statring_button.style.boxShadow = `5px 2px 5px rgba(0, 0, 0, 0.5)`
+
+        anyWordButton.style.backgroundColor = '';
+        anyWordButton.style.padding = '';
+        anyWordButton.style.fontWeight = '';
+        anyWordButton.style.boxShadow = '';
+    }
+});
+
+anyWordButton.addEventListener('click',function() {
+    startsWith = false;
+    anyWord = true;
+    if(anyWord) {
+        anyWordButton.style.backgroundColor = `rgb(164, 89, 255)`
+        anyWordButton.style.padding = `0.6rem`
+        anyWordButton.style.fontWeight = `bold`
+        anyWordButton.style.boxShadow = `5px 2px 5px rgba(0, 0, 0, 0.5)`
+
+        statring_button.style.backgroundColor = ''; 
+        statring_button.style.padding = '';
+        statring_button.style.fontWeight = '';
+        statring_button.style.boxShadow = '';
+    }
+})
+
+inputEl.addEventListener('input', function() {
+    const inputValue = inputEl.value.trim().toLowerCase();
+    
+    if (startsWith) {
+        const filteredCountries = countries.filter(country => country.toLowerCase().startsWith(inputValue));
+        create_country_card(filteredCountries);
+        
+        let filteredLen = filteredCountries.length;
+        let value = inputEl.value;
+        
+        starts_or_contain.style.display = 'block';
+        starts_or_contain.innerHTML = `Countries start with <span class="value">${value}</span> are <span class="filteredLen">${filteredLen}</span>`;
+    } else if (anyWord) {
+        const filteredCountries = countries.filter(country => country.toLowerCase().includes(inputValue));
+        create_country_card(filteredCountries);
+        
+        let filteredLen = filteredCountries.length;
+        let value = inputEl.value;
+        
+        starts_or_contain.style.display = 'block';
+        starts_or_contain.innerHTML = `Countries contain <span class="value">${value}</span> are <span class="filteredLen">${filteredLen}</span>`;
+    }
+});
