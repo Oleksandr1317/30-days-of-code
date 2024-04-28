060f8b8831318799c1003e40a893c1145a0a2905
general <sgladkoskoka@gmail.com>
2024-04-28 14:43:58 +0300

    Days 27 finished

--- 27 Days (portfolio)/js/main.js
+++ 27 Days (portfolio)/js/main.js
@@ -16,3 +16,26 @@ changingText.addEventListener('animationiteration', () => {
     }
     iconElement.textContent = icon[currentIndex]
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const main_text = document.querySelector('.main-text');
+    if (main_text) {
+        main_text.innerHTML = `This is a place for <span id='span-text-content'>text content</span>.`;
+    } else {
+        console.error("Element with class '.main-text' not found.");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const jsProjects = document.getElementById('java-script-projects');
+    const otherProjects = document.getElementById('other-project');
+
+    jsProjects.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Oleksandr1317/30-days-of-code';
+    });
+
+    otherProjects.addEventListener('click', function() {
+        window.location.href = 'https://github.com/Oleksandr1317?tab=repositories';
+    });
+});
+
